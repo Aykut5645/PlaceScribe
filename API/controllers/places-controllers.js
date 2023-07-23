011491bb8633d49ab39f34cc0efe73ca2cacdcb9
Aykut5645 <aykut.fatin@abv.bg>
2023-07-23 15:57:02 +0300

    Used Geocoding Google API in app to convert address into coordinates

--- API/controllers/places-controllers.js
+++ API/controllers/places-controllers.js
@@ -2,6 +2,7 @@ const uuid = require('uuid').v4;
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = [{
     id: 'p1',
@@ -43,7 +44,7 @@ const getPlacesByUserId = (req, res, next) => {
     res.json({ places: [ ...currentPlaces ] });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return next(
@@ -51,7 +52,15 @@ const createPlace = (req, res, next) => {
         );
     }
 
-    const { title, description, coordinates, address, creator } = req.body;
+    const { title, description, address, creator } = req.body;
+    let coordinates;
+
+    try {
+        coordinates = await getCoordsForAddress(address);
+    } catch(error) {
+        return next(error);
+    }
+
     const createdPlace = {
         id: uuid(),
         title,
@@ -60,6 +69,7 @@ const createPlace = (req, res, next) => {
         address,
         creator
     };
+
     DUMMY_PLACES.push(createdPlace);
     res.status(201).json({ place: createdPlace });
 };
