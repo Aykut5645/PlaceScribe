09a22b54ebbd1e89dfac598278ff270e408c8a4f
Aykut5645 <aykut.fatin@abv.bg>
2023-07-24 09:40:48 +0300

    Solved a bug

--- API/util/location.js
+++ API/util/location.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const HttpError = require("../models/http-error");
-const API_KEY = 'AIzaSyCBS7DZswLOliZAFeAGTbTSG4nPOK9pYFc';
+const API_KEY = '';
 
 const getCoordsForAddress = async (address) => {
     const { data } = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
