4bb4fab4e92841f82babff339ebdfdeea455653a
Aykut5645 <aykut.fatin@abv.bg>
2023-08-14 19:19:13 +0300

    Added input validation into register form

--- SPA/src/app/auth/register/register.component.ts
+++ SPA/src/app/auth/register/register.component.ts
@@ -21,15 +21,30 @@ export class RegisterComponent implements OnInit {
         this.passwordInputType = type;
     }
 
+    get emailIsValid(): boolean {
+        return Boolean(this.registerForm.get('email')?.invalid && this.registerForm.get('email')?.touched);
+    }
+
+    get passwordIsValid(): boolean {
+        return Boolean(this.registerForm.get('password')?.invalid && this.registerForm.get('password')?.touched);
+    }
+
+    get isMatch(): boolean {
+        return Boolean(
+            this.registerForm.get('password')?.valid &&
+            this.registerForm.get('confirmPassword')?.valid &&
+            this.registerForm.get('password')?.value !== this.registerForm.get('confirmPassword')?.value
+        );
+    }
+
     submitHandler(): void {
         console.log('Register => ', this.registerForm.value);
-
     }
 
     private createForm(): void {
         this.registerForm = this.fb.group({
-            email: [null, [Validators.required]],
-            password: [null, [Validators.required]],
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, [Validators.required, Validators.minLength(6)]],
             confirmPassword: [null, [Validators.required]],
         });
     }
