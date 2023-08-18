75f1338bd9594661c0af8107121b35f48c48ce00
Aykut5645 <aykut.fatin@abv.bg>
2023-08-18 13:47:03 +0300

    Added mimeType validator

--- SPA/src/app/auth/register/register.component.ts
+++ SPA/src/app/auth/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
+
 import { AuthApiActions } from '../+state/actions';
+import { mimeType } from '../../mime-type.validator';
 
 @Component({
     selector: 'app-register',
@@ -68,7 +70,7 @@ export class RegisterComponent implements OnInit {
 
     private createForm(): void {
         this.registerForm = this.fb.group({
-            image: [null, [Validators.required]],
+            image: [null, { validators: [Validators.required], asyncValidators: [mimeType] }],
             name: [null, [Validators.required]],
             email: [null, [Validators.required, Validators.email]],
             password: [null, [Validators.required, Validators.minLength(6)]],
